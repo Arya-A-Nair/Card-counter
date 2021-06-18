3775ef33b20fc8bae23d3dc648094008ce8f16d5
Arya-A-Nair <aryaajitnair@gmail.com>
2021-06-19 00:48:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 var count = 0;
+function cc() {
+    var x = document.getElementById("btn1").value;   
+    
+    switch(x){
+    case '1':
+    
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    count++;
+    break;
+    case '10':
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+    count--;
+    break;
+    }
 
-function cc(card) {
- 
-  
-switch(card){
-  case 1:
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-  case 6:
-  count++;
-  break;
-  case 10:
-  case 'J':
-  case 'Q':
-  case 'K':
-  case 'A':
-  count--;
-  break;
-}
+
 var decision="Hold";
+
 if(count>0){
   decision="Bet";
 }
 document.getElementById('result').innerHTML=count+' '+decision;
-
- 
 }
 
