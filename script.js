6d4a22731adfef3f98a7ed8b09b311a01e71cb9a
Arya-A-Nair <aryaajitnair@gmail.com>
2021-06-18 23:02:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var count = 0;
 
 function cc(card) {
-  // Only change code below this line
+ 
   
 switch(card){
   case 1:
@@ -26,7 +26,7 @@ if(count>0){
 }
 return count+' '+decision;
 
-  // Only change code above this line
+ 
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
